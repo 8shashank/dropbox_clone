f8518523bc2475a7f9ca0da08d7b6bf376c32d28
collinjackson93 <collinjackson93@gmail.com>
2015-09-07 18:18:11 -0500

    Removed code duplication for the text that is logged

--- QuestionGUIServer.js
+++ QuestionGUIServer.js
@@ -21,14 +21,13 @@ app.post('/Submit', function(request, response){
             response.write('Thank you for your business, ' + fields.userName + '!<br />');
             response.write('Your Comment has been processed. Please expect a reply within 24 hours.');
             response.end(myHTML + final);
-            console.log("At timestamp " + today.toUTCString() + ",");
-            console.log("server received comment from user " + fields.userName + ", who commented:");
-            console.log("\"" + fields.textArea + "\"");
-            console.log("Reply to " + fields.userName + " at " + fields.userEmail);
+            var outputString = "\nAt timestamp " + today.toUTCString() + ", user " +
+                                fields.userName + " with email " + fields.userEmail +
+                                " commented: " + fields.textArea;
+            console.log(outputString);
 
             var path = 'commentlog.txt';
-            var buffer = new Buffer("At timestamp " + today.toUTCString() + ", user "
-                + fields.userName + " with email " + fields.userEmail + " commented: " + fields.textArea + "\n");
+            var buffer = new Buffer(outputString);
 
             /** Honor Citation: learned how to write to files with node.js (and borrowed some code from) the following link
              * http://stackoverflow.com/questions/2496710/writing-files-in-node-js
--- REVIEW.md
+++ REVIEW.md
@@ -8,3 +8,5 @@ No changes were made to the existing code. The additions consist of a simple web
 
 *Provide a list of 5+ aspects of the code that should be improved. Each suggestion for improvement should be accompanied by a GitHub pull request on the reviewee's submission repo that shows how to perform the suggested refactoring. If the change is so substantial that it is "rewriting" the solution, break it down into a series of refactorings that build on each other to improve the solution (each refactoring committed separately and submitted as a pull request with a thorough explanation).*
 1. Change the location of `commentlog.txt`. If the directory `ClientMessageLog` does not exist, attempting to open the file results in an error.
+
+2. Remove redundancy of what is logged to the console and what is written to `commentlog.txt`.
