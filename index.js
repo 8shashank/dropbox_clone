227eb80aa599ca32724987c9466b0fb87403f848
Nick Kline <nicolas.m.kline@vanderbilt.edu>
2015-09-07 19:16:49 -0500

    REDO First Fix

--- index.js
+++ index.js
@@ -53,9 +53,7 @@ var syncFile = function(fromPath,toPath){
                         throw err;
 
                 });
-                //second improvement
-                console.log("File has been copied and log has been updated.")
-            });
+             });
             /*fs.stat('Log.txt', function(err, stat) {
                if (err == null) {
                    //file exists
