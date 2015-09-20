47ffff955ee2edc0156d02dec3d2696830de5b41
shashank_sharma_ <8shashank@gmail.com>
2015-09-20 18:08:14 -0500

    Added more tests

--- test/test.js
+++ test/test.js
@@ -6,10 +6,35 @@ var dropboxIgnore="_.dropboxignore";
 //var syncClient=require('../lib/sync/sync-client');
 //var syncServer=require('../lib/sync/sync-server');
 
+describe('dropboxURIs', function(){
+    var uris = require("../lib/sync/dropboxuris");
+    describe('getProtocol', function(){
+        it('should correctly parse protocols from full uris', function(){
+            expect(uris.getProtocol("foo://asdf.com?asdf3=2")).to.equal("foo");
+        });
+
+        it('should default to file if no scheme is present in uri', function(){
+            expect(uris.getProtocol("/foo/bar")).to.equal("file");
+        });
+    });
+
+    describe('getPath', function(){
+        it('should correctly parse path from full uris', function(){
+            expect(uris.getPath("foo:///foo/bar")).to.equal("/foo/bar");
+        });
+
+        it('should correctly parse path if no protocol in uri', function(){
+            expect(uris.getPath("/foo/bar")).to.equal("/foo/bar");
+        });
+    });
+
+});
+
 describe('sync', function()
 {
+    var sync=require('../lib/sync/sync');
+
     describe('#fsHandlers', function () {
-        var sync=require('../lib/sync/sync');
         before(function() {
             fs.mkdirSync("test-temp");
             fs.mkdirSync("test-temp/folder1");
@@ -63,123 +88,146 @@ describe('sync', function()
         });
 
     });
+
+    describe('#comparisons', function(){
+        var stat1 = {size:120};
+        var stat2 = {size:200};
+        var stat3 = {size:120};
+
+        it('Should return falsy value such as undefined if second file is undefined', function(){
+            expect(sync.filesMatchName(stat1,undefined)).to.not.be.ok;
+        })
+
+        it('Files having different sizes should not match', function(){
+            expect(sync.filesMatchNameAndSize(stat1,stat2)).to.not.equal.true;
+
+        });
+
+        it('Files having same sizes should match', function(){
+            expect(sync.filesMatchNameAndSize(stat1,stat3)).to.not.equal.true;
+        });
+
+    })
 });
 
 describe('sync-driver', function(){
     var syncDriver=require('../lib/sync/sync-driver');
     var sync=require('../lib/sync/sync');
-    var argv=
-    {
-        directory1: "test-temp/folder1",
-        directory2:"test-temp/folder2"
-    };
 
     //When both files have a dropboxIgnore
     describe('#checkForChanges1', function(){
+        var mainFolder="test-temp-1";
         var argv=
         {
-            directory1: "test-temp/folder1",
-            directory2:"test-temp/folder2"
+            directory1: mainFolder+"/folder1",
+            directory2:mainFolder+"/folder2"
         };
 
+        var folder1=argv.directory1;
+        var folder2=argv.directory2
+
         before(function() {
-            fs.mkdirSync("test-temp");
-            fs.mkdirSync("test-temp/folder1");
-            fs.mkdirSync("test-temp/folder2");
-            fs.writeFileSync("test-temp/folder1/test.txt");
+            fs.mkdirSync(mainFolder);
+            fs.mkdirSync(folder1);
+            fs.mkdirSync(folder2);
+            fs.writeFileSync(folder1+"/test.txt");
 
-            fs.writeFileSync("test-temp/folder1/ignoreme11.txt", "folder 1");
-            fs.writeFileSync("test-temp/folder1/ignoreme12.txt");
-            fs.writeFileSync("test-temp/folder2/ignoreme21.txt");
-            fs.writeFileSync("test-temp/folder2/ignoreme11.txt", "folder 2");
+            fs.writeFileSync(folder1+"/ignoreme11.txt", "folder 1");
+            fs.writeFileSync(folder1+"/ignoreme12.txt");
+            fs.writeFileSync(folder2+"/ignoreme21.txt");
+            fs.writeFileSync(folder2+"/ignoreme11.txt", "folder 2");
 
-            fs.writeFileSync("test-temp/folder1/"+dropboxIgnore, "ignoreme11.txt\nignoreme12.txt\nignoreme21.txt");
-            fs.writeFileSync("test-temp/folder2/"+dropboxIgnore, "ignoreme21.txt");
+            fs.writeFileSync(folder1+"/"+dropboxIgnore, "ignoreme11.txt\nignoreme12.txt\nignoreme21.txt");
+            fs.writeFileSync(folder2+"/"+dropboxIgnore, "ignoreme21.txt");
         });
 
         after(function(){
-             fs.unlinkSync("test-temp/folder1/test.txt");
-             fs.unlinkSync("test-temp/folder2/test.txt");
+            fs.unlinkSync(folder1+"/test.txt");
+            fs.unlinkSync(folder2+"/test.txt");
 
-             fs.unlinkSync("test-temp/folder1/ignoreme11.txt");
-             fs.unlinkSync("test-temp/folder1/ignoreme12.txt");
-             fs.unlinkSync("test-temp/folder2/ignoreme21.txt");
-             fs.unlinkSync("test-temp/folder2/ignoreme11.txt");
+            fs.unlinkSync(folder1+"/ignoreme11.txt");
+            fs.unlinkSync(folder1+"/ignoreme12.txt");
+            fs.unlinkSync(folder2+"/ignoreme21.txt");
+            fs.unlinkSync(folder2+"/ignoreme11.txt");
 
-             fs.unlinkSync("test-temp/folder1/"+dropboxIgnore);
-             fs.unlinkSync("test-temp/folder2/"+dropboxIgnore);
+            fs.unlinkSync(folder1+"/"+dropboxIgnore);
+            fs.unlinkSync(folder2+"/"+dropboxIgnore);
 
-             fs.rmdirSync("test-temp/folder1");
-             fs.rmdirSync("test-temp/folder2");
-             fs.rmdirSync("test-temp");
+             fs.rmdirSync(folder1);
+             fs.rmdirSync(folder2);
+             fs.rmdirSync(mainFolder);
         });
 
         it('should have test.txt synced to folder2', function(){
             syncDriver.checkForChanges(sync,argv);
-            var files=fs.readdirSync("test-temp/folder2");
+            var files=fs.readdirSync(folder2);
             expect(files).to.include("test.txt");
         });
 
         //This file is present in folder1 but not in folder2.
         it('should not sync ignoreme12.txt to folder2', function(){
             syncDriver.checkForChanges(sync,argv);
-            expect(fs.readdirSync("test-temp/folder2")).to.not.include("ignoreme12.txt");
+            expect(fs.readdirSync(folder2)).to.not.include("ignoreme12.txt");
         })
 
         //This file is present in both folders
         it('should not sync ignoreme11.txt to or from folder2', function(){
             syncDriver.checkForChanges(sync,argv);
-            expect(fs.readFileSync("test-temp/folder2/ignoreme11.txt", {encoding: 'utf8'})).to.not.include("folder1");
-            expect(fs.readFileSync("test-temp/folder1/ignoreme11.txt", {encoding: 'utf8'})).to.not.include("folder2");
+            expect(fs.readFileSync(folder2+"/ignoreme11.txt", {encoding: 'utf8'})).to.not.include("folder1");
+            expect(fs.readFileSync(folder1+"/ignoreme11.txt", {encoding: 'utf8'})).to.not.include("folder2");
         })
 
         //This file is ignored in folder1 but not present there
         it('should not sync ignoreme21.txt to folder1', function(){
             syncDriver.checkForChanges(sync,argv);
-            expect(fs.readdirSync("test-temp/folder1")).to.not.include("ignoreme21.txt");
+            expect(fs.readdirSync(folder1)).to.not.include("ignoreme21.txt");
         })
     })
 
     //When only one folder has a dropboxIgnore
     describe('#checkForChanges#2', function(){
+        var mainFolder="test-temp-2";
         var argv=
         {
-            directory1: "test-temp-2/folder1",
-            directory2:"test-temp-2/folder2"
+            directory1: mainFolder+"/folder1",
+            directory2:mainFolder+"/folder2"
         };
 
+        var folder1=argv.directory1;
+        var folder2=argv.directory2;
+
         before(function(){
-            fs.mkdirSync("test-temp-2");
-            fs.mkdirSync("test-temp-2/folder1");
-            fs.mkdirSync("test-temp-2/folder2");
-            fs.writeFileSync("test-temp-2/folder1/test11.txt","folder1");
-            fs.writeFileSync("test-temp-2/folder1/test12.txt");
-            fs.writeFileSync("test-temp-2/folder1/"+dropboxIgnore, "test11.txt");
+            fs.mkdirSync(mainFolder);
+            fs.mkdirSync(folder1);
+            fs.mkdirSync(folder2);
+            fs.writeFileSync(folder1+"/test11.txt","folder1");
+            fs.writeFileSync(folder1+"/test12.txt");
+            fs.writeFileSync(folder1+""+"/"+dropboxIgnore, "test11.txt");
         });
 
         after(function(){
-            fs.unlinkSync("test-temp-2/folder1/test11.txt");
-            fs.unlinkSync("test-temp-2/folder1/test12.txt");
-            fs.unlinkSync("test-temp-2/folder2/test12.txt");
-            fs.unlinkSync("test-temp-2/folder1/"+dropboxIgnore);
-            fs.rmdirSync("test-temp-2/folder1");
-            fs.rmdirSync("test-temp-2/folder2");
-            fs.rmdirSync("test-temp-2");
+            fs.unlinkSync(folder1+"/test11.txt");
+            fs.unlinkSync(folder1+"/test12.txt");
+            fs.unlinkSync(folder2+"/test12.txt");
+            fs.unlinkSync(folder1+""+"/"+dropboxIgnore);
+            fs.rmdirSync(folder1);
+            fs.rmdirSync(folder2);
+            fs.rmdirSync(mainFolder);
         });
 
         it('should sync test12.txt to folder2', function(){
             syncDriver.checkForChanges(sync,argv);
-            expect(fs.readdirSync("test-temp-2/folder1")).to.include("test12.txt");
+            expect(fs.readdirSync(folder2)).to.include("test12.txt");
         });
 
         it('should not sync test11.txt to folder2', function(){
             syncDriver.checkForChanges(sync,argv);
-            expect(fs.readdirSync("test-temp-2/folder2")).to.not.include("test11.txt");
+            expect(fs.readdirSync(folder2)).to.not.include("test11.txt");
         });
 
         it('should not sync dropboxIgnore file to folder2', function(){
             syncDriver.checkForChanges(sync,argv);
-            expect(fs.readdirSync("test-temp-2/folder2")).to.not.include("_.dropboxignore");
+            expect(fs.readdirSync(folder2)).to.not.include("_.dropboxignore");
         })
     })
 });
@@ -188,6 +236,7 @@ describe('pipeline', function(){
     var Pipeline=require('../lib/sync/pipeline').Pipeline;
     var action1={exec: function(num){ return num + 1;}};
     var action2={exec: function(num) {return num * 3;}};
+    var testNum=5;
 
     it('should include exec function', function(){
         expect(Pipeline.prototype.hasOwnProperty("exec")).to.be.true;
@@ -197,7 +246,7 @@ describe('pipeline', function(){
         expect(Pipeline.prototype.hasOwnProperty("addAction")).to.be.true;
     });
 
-    it('action should have exec function', function(){
+    it('Pipeline should only accept action with exec function', function(){
         var pipeline=new Pipeline();
         var badFunction=function(){
             pipeline.addAction({test:function(){console.log("This should throw")}});
@@ -205,33 +254,39 @@ describe('pipeline', function(){
         expect(badFunction).to.throw("Actions must have an exec() function");
     });
 
+    it('Pipeline should remember actions', function(){
+        var pipeline=new Pipeline();
+        pipeline.addAction(action1);
+        pipeline.addAction(action2);
+        expect(pipeline.actions.length).to.equal(2);
+    });
+
+    it('Pipeline should calculate and return a result object', function(){
+        var pipeline=new Pipeline();
+        pipeline.addAction(action1);
+        assert(pipeline.exec(testNum).result===action1.exec(testNum));
+    })
+
     it('Pipeline should execute all actions', function(){
         var pipeline=new Pipeline();
         pipeline.addAction(action1);
         pipeline.addAction(action2);
-        var num=5;
-        var result=pipeline.exec(num).result;
-        expect(result).to.equal((num+1)*3);
+        var result=pipeline.exec(testNum).result;
+        expect(result).to.equal(action2.exec(action1.exec(testNum)));
     });
 
     it('Pipeline should remember history', function(){
         var pipeline=new Pipeline();
         pipeline.addAction(action1);
         pipeline.addAction(action2);
-        var num=5;
-        var history=pipeline.exec(num).history;
-        assert(history.length===3);
-        assert(history[0]===num);
-        assert(history[1]===++num);
-        assert(history[2]===(num*3));
-        expect(true).to.not.equal(false);
-    });
+        var action1result=action1.exec(testNum);
 
-    it('Pipeline should remember actions', function(){
-        var pipeline=new Pipeline();
-        pipeline.addAction(action1);
-        pipeline.addAction(action2);
-        expect(pipeline.actions.length).to.equal(2);
+        var history=pipeline.exec(testNum).history;
+        assert(history.length===3);
+        assert(history[0]===testNum);
+        assert(history[1]===action1result);
+        assert(history[2]===action2.exec(action1result));
+        expect(true).to.not.equal(false);
     });
 });
 
@@ -250,8 +305,8 @@ describe('sync server/client', function(){
         var conn = d.connect(host,port);
 
         conn.on('remote', function (remote) {
-            done();
             d.end();
+            done();
         });
     });
 
